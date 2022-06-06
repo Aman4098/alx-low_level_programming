6ba5ccc7a745b8f049f0f69b7e0f275636e219b8
Aman4098 <amanualdawit1978@gmail.com>
2022-06-06 01:04:27 -0700

    my last commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- program that prints all possible combinations of single-digit numbers
+ * return: always 0
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 48; ch <= 57; ch++)
+	{
+
+	        putchar(ch);
+		if (ch != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+											}		
+
+			putchar(10);
+	return (0);
+}
+	
