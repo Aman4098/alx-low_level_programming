e3eff529ad259066afdede9952efbb0a2153ba2b
Aman4098 <amanualdawit1978@gmail.com>
2022-06-07 16:09:08 -0700

    my second commit' ' '

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *main -prints the alphabet, in lowercase,
+ *Return - always o
+ */
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a' ; j <= 'z'; J++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n')
+}
+
+
