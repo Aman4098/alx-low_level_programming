f386c00f2c905ba541f564bed0bf7e33108da624
Aman4098 <amanualdawit1978@gmail.com>
2022-06-08 11:27:34 -0700

    my commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -22,9 +22,9 @@ void jack_bauer(void)
 			_putchar((i % 10) + '0');
 		if (j < 10 )
 		{
-			_putchar(':')
-			_putchar('0')
-			_putchar(j + '0' )
+			_putchar(':');
+			_putchar('0');
+			_putchar(j + '0');
 		}
 		else if (j > 10)
 		{
