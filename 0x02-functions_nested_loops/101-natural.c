572120f2efbce0ac160282b06f60c1d46cf689f5
Aman4098 <amanualdawit1978@gmail.com>
2022-06-08 14:46:57 -0700

    my commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 	int i;
-	unsigned long int j ,k next, sum;
+	unsigned long int j ,k , next, sum;
 	j = 1;
 	k = 2;
 	sum = 0;
 	for (i = 1; i <= 33; ++i)
 	{
-		if (j < 4000000 && (j % 2) == 00
+		if (j < 4000000 && (j % 2) == 00)
 				{
 				sum =sum = j;
 				}
@@ -24,4 +24,5 @@ int main(void)
 				}
 
 				printf("%lu\n",sum);
-				return (0)
+				return (0);
+}
