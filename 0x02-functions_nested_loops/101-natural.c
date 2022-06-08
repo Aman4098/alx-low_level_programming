33d095fc856dcaf430a9f4814a212bc1d7a34757
Aman4098 <amanualdawit1978@gmail.com>
2022-06-08 14:40:35 -0700

    my commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main -finds and prints the sum of the even-valued terms
+ * followed by new line
+ * Return: alwayes 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long int j ,k next, sum;
+	j = 1;
+	k = 2;
+	sum = 0;
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 00
+				{
+				sum =sum = j;
+				}
+				next = J + k;
+				j = k;
+				k = next;
+				}
+
+				printf("%lu\n",sum);
+				return (0)
