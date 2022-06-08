513c6717542e65e7bc3ef2d5965e5ffb55e9dc5a
Aman4098 <amanualdawit1978@gmail.com>
2022-06-08 12:58:06 -0700

    my commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+ #include "main.h"
+
+/**
+ * times _table time table
+ */
+void times_table(void)
+{
+	int x, y, K;
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			k = x * Y;
+			if (y == 0)
+				_putchar(k +'0');
+			if (y != 0 && k < 10)
+			{
+				_putchar(',');
+			       _putchar(' ');
+		        	_putchar(' ');
+		 		_putchar(k +'0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k % 10) + '0');
+				_putchar((k / 10) + '0');
+			}
+		}
+		-putchar('\n');
+	}
+}
+
+
+
