c8d1e4ca57b396cdf36ea58cd9c724bf8675777f
Aman4098 <amanualdawit1978@gmail.com>
2022-06-10 10:35:46 -0700

    mu commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,9 +12,9 @@ void print_line(int n)
 		_putchar('\n');
 	else
 	{		
-		for (x = 0; x < n; x++)
+			for (x = 0; x < n; x++)
 			_putchar(95);
-			_putchar('\n');
+		_putchar('\n');
 	
 	}	
 }
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_diagonal -> print diagonal line
+ * @size: integer paragme
+ */
+void print_diagonal(int size)
+{
+	int x;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (i = 0; i <= x; i++)
+				_putchar('_');
+			_putchar(92);
+			_pitchar('\n')
+
+
+
+
