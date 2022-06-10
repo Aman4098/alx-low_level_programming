036b724cd5fa6f81c1f900571c92d9b1e36f57cd
Aman4098 <amanualdawit1978@gmail.com>
2022-06-10 11:28:31 -0700

    my commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,15 +7,14 @@
 void print_square(int size)
 {
 	int x, y;
-	int size = 4;
 
 	if (size <= 0)
 		_putchar('\n');
 	else
 	{
-		for (x = 0; x < size : x++)
+		for (x = 0; x < size; x++)
 		{
-			for (y = 0, y < size : y++)
+			for (y = 0;  y < size; y++)
 			{
 			_putchar(35);
 			}
