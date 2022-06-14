58d77b144f40ed0e7a5bd931317b6193fdabd88b
Aman4098 <amanualdawit1978@gmail.com>
2022-06-14 16:21:02 -0700

    my commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rest_to_ 98 -> a function to update the  reference of a pointer
+ * reset_to_ 98 -> a function to update the  reference of a pointer
  * @n: pointer to n to be pointed
  */
 void reset_to_98(int *n)
