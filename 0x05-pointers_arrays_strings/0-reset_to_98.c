b6e40fe898d313ee9d774b7bc486dcd3b4581f9a
Aman4098 <amanualdawit1978@gmail.com>
2022-06-14 16:16:30 -0700

    my hot fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *rest_to_ 98 -> afunction to update thre reference of a pointer
- *@n: pointer to n to be pointed
+ * rest_to_ 98 -> afunction to update thre reference of a pointer
+ * @n: pointer to n to be pointed
  */
-void reset_to_98(int *n) 
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
