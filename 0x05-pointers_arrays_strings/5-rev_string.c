1208585368fe2174d690c68fbe97788f96e83ffb
Aman4098 <amanualdawit1978@gmail.com>
2022-06-14 22:58:28 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string -> reversing a string
+ * @s: parameters s
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, length 1, length 2;
+
+	length 1 = 0;
+	length 2 = 0;
+
+	while (s[length1] != '\0')
+		length1++;
+	length2 = length1 - 1
+		for (i = 0; i < length / 2; i++)
+				tmp = s[i];
+				s[i] = s[length2];
+				s[length2] = tmp;
+				length2 - = 1;
+}
+}
