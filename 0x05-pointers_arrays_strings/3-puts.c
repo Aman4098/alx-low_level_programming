8b0a2e3b3d1bd4ea76ed576d3e1132f1e401ca34
Aman4098 <amanualdawit1978@gmail.com>
2022-06-14 18:05:27 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _streln -> function to get the lengeth of a string
+ *@s: string pointer to passed to this function
+ *Return: returns the lengeth of a strring
+ */
+void _puts(char *s)
+{
+	int len;
+	return (strlen(s));
+}
+ 
