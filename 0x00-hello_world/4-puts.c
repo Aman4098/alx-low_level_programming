f61505a03d6b53dbf8ab36b0fac093b138967a30
Aman4098 <amanualdawit1978@gmail.com>
2022-06-15 06:57:55 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
+/*
+ * file: 4 puts.c
+ * auth: 
+ */
 #include <stdio.h>
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
