8c53fca748d79c7b8e29ba6af9bf81aec9260256
Aman4098 <amanualdawit1978@gmail.com>
2022-06-15 05:46:36 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 -> puts 2 function
+ * @str: sting param
+ */
+void puts2(char *str)
+{
+	int len;
+
+	len = strlen(str)
+	for (i = 0; i < len; i++)
+		_putchar(str[i]);
+}
