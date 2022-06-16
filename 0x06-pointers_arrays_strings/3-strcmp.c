356a8d05402a90e141c45152a7d128b87da59fc6
Aman4098 <amanualdawit1978@gmail.com>
2022-06-16 04:48:19 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp -> for comparing purpouse
+ * @s1: param 1
+ * @s2 param 2
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = srtcmp(sl, s2);
+	return (result);
+}
