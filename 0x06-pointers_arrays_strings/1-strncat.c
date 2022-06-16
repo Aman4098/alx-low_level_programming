13e0f0795097cf810ac747b3b7f016d45c40df3c
Aman4098 <amanualdawit1978@gmail.com>
2022-06-16 04:09:06 -0700

    mycommit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ *_strncat  -> function to spend some charx
+ *@dest: first param
+ *@src:second para
+ *@n: thirde para
+ *Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(*dest, *src, n)
+}
