8948c218db380b061fea641c97262826153f7356
Aman4098 <amanualdawit1978@gmail.com>
2022-06-16 13:23:24 -0700

    my commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,16 +4,15 @@
  *main ->Write a program that prints all single digit numbers of base
  *10 starting from
  *followed by a new line.
-*Return: alwayes 0
+ *Return: alwayes 0
  */
 int main(void)
 {
 	int n;
 
 		for (n = 0 ; n < 10; n++)
-		{
-			printf("%d", n);
-		}
-	printf("\n");
+		
+		printf("%d", n);
+		printf("\n");
 		return (0);
 }
