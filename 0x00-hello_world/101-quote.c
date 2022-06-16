cf32be6319f1c6300d7dfe88eaab7f91129105ba
Aman4098 <amanualdawit1978@gmail.com>
2022-06-16 08:54:29 -0700

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-
 #include <unistd.h>
-
 /**
  * main - prints exactly and that piece of art is useful" 
- * - Dora Korpar, 2015-10-19
+ * - Dora Korpar, 2015-10-19",
  *   followed by a new line, to the standard error.
- * return - always 0
+ * Return - always 0
  */
  int main(void)
 {
-	printf("and that piece of art is useful" - Dora Korpar, 2015-10-19 \n",)
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19 \n", 59 )
 		
   return (1);
 }
