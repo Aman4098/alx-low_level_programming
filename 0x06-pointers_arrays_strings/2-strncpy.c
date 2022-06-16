c4604c0d6d39672a216cb1e5cc07a8427f331d86
Aman4098 <amanualdawit1978@gmail.com>
2022-06-16 04:29:59 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strncpy -> for coping purpouse
+ *@dest: parame 1
+ *@src: para 2 
+ *@n: para 3
+*Return: string
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
