17b670905a2572a93eaa1b247b11c0d8ba66f4d6
Aman4098 <amanualdawit1978@gmail.com>
2022-09-10 19:16:45 -0700

    my commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: alwayes(0)
+ */
+int main(void)
+{
+	int k;
+	for (k = 48; k <= 57; k++)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return(0)
+}
